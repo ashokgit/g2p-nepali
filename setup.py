c41dc5994d501f2d91f4f2efffedf6c90a269e28
Ashok Poudel <ashok.poudel@gmail.com>
2023-04-19 08:55:59 +0545

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='nepali_g2p',
+    version='0.1.0',
+    description='A simple Nepali G2P conversion library',
+    author='Your Name',
+    author_email='youremail@example.com',
+    url='https://github.com/yourusername/nepali_g2p',
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+)
